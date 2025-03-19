24307387dc7a8060b6b67090504f2e94a53ef7c4
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-19 04:27:31 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+
+
+  const taskList = document.getElementById('tasks');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskDescription = document.getElementById('new-task-description').value.trim();
+    if (taskDescription !== '') {
+      const li = document.createElement('li');
+
+      li.textContent = taskDescription;
+      taskList.appendChild(li);
+      form.reset()
+    }
+  })
+
 });
