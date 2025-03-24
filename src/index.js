e2ad793b8c7afc2ddd625fcafc3f99c3fe65b8b3
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-24 08:27:45 +0000

    adds main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-\  const form = document.getElementById('create-task-form');
+ const form = document.getElementById('create-task-form');
 
 
   const taskList = document.getElementById('tasks');
